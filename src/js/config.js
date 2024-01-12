83ca19a86e0a246698627ed41289995b29a5e921
minamaximous11 <mena92591@gmail.com>
2024-01-12 07:04:45 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'd0472adb-406a-4617-8c4f-5f3bcf4dcc2c';
+export const MODAL_CLOSE_SEC = 2.5;
